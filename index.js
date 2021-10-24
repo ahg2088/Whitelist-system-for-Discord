6ce4db948523a337f74a9dcce2b608dd482e88a5
ahg2088 <87061093+ahg2088@users.noreply.github.com>
2021-10-24 17:07:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ client.on("guildMemberAdd", async function(member) {
             member.ban().then((user) => {
                 // Successmessage
                 client.channels.cache
-                .get(`698285641880043551`)
-                .send('@everyone alerta!!!! alguien agrego el bot llamado ' + member.user.tag + " ID= " + member.user.id);
+                .get(`698285641880043551`) //Add id of the channel where you will notify about the entry of the bot
+                .send('@everyone alerta!!!! alguien agrego el bot llamado ' + member.user.tag + " ID= " + member.user.id); //Message the bot will say
             }).catch(() => {
                 // Failmessage
                 console.log("Access Denied");
@@ -38,7 +38,7 @@ client.on("guildMemberAdd", async function(member) {
 });
 
 client.on("message", async message => {
-    let prefix = "r/";
+    let prefix = "r/"; //Your Prefix
     if (message.author.bot) return;
     // This is where we'll put our code.
     if (message.content.indexOf(prefix) !== 0) return;
@@ -47,7 +47,7 @@ client.on("message", async message => {
     const command = args.shift().toLowerCase();
 
     if(command === "addbot") {
-        if(message.member.roles.cache.find(r => r.name ===  "《🌠》➺┇ Manager")){
+        if(message.member.roles.cache.find(r => r.name ===  "《🌠》➺┇ Manager")){ //Name of the role that will be allowed to execute the command
         let ids = args[0];
         if (!args.length)
         return message.channel.send('Necesitas poner la id del bot que quieres agregar');
@@ -55,18 +55,18 @@ client.on("message", async message => {
         if(!tiene) {
             try {
                 CreateWhitelist(ids);
-                message.channel.send("El bot se registro con exito")
+                message.channel.send("El bot se registro con exito") //It will send this message if the bot was added correctly
                 .catch(console.error);
               }catch (error) {
-                message.channel.send("No se puede registrar el bot en estos momentos");
+                message.channel.send("No se puede registrar el bot en estos momentos"); //It will send this message if there was an error adding the bot
               }
         } else {
-            message.channel.send("Este bot ya esta en la whitelist");
+            message.channel.send("Este bot ya esta en la whitelist"); //In case it is already added
         }
           } else {
-              message.reply("Andate a la ctm que no tienes permiso");
+              message.reply("No tienes el rol necesario"); //In case the user who executed the command does not have the role
           }
     }
 })
 
-client.login('Token');
+client.login('Token'); // Your token
